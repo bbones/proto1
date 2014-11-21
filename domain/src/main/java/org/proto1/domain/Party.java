87cec11d97ed85d6828cf649a07d9c54f448df46
pva <pva@isd.comua>
2014-11-21 17:55:24 +0200

    JSON request/response

--- domain/src/main/java/org/proto1/domain/Party.java
+++ domain/src/main/java/org/proto1/domain/Party.java
@@ -1,5 +1,7 @@
 package org.proto1.domain;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GenerationType;
--- protofront/src/main/java/org/proto1/protofront/EnterpriseController.java
+++ protofront/src/main/java/org/proto1/protofront/EnterpriseController.java
@@ -1,14 +1,25 @@
 package org.proto1.protofront;
 
+import org.proto1.domain.Enterprise;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/enterprise")
 public class EnterpriseController {
-	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
+	@RequestMapping(value = "submit", method = RequestMethod.POST)
+	public @ResponseBody Enterprise submit(@RequestBody final Enterprise enterprise) {
+		return  new Enterprise();
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public String sayHello(@PathVariable String name) {
 		String result = "Hello " + name + " to dineshonjava.com!!!";
 		return result;
