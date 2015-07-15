1f8c9127fe9dec510176f804d20a49da61fb0242
pva <pva@isd.comua>
2015-07-15 18:24:48 +0300

    : Request
    Task-Url:

--- domain/src/main/java/org/proto1/domain/order/Request.java
+++ domain/src/main/java/org/proto1/domain/order/Request.java
@@ -6,7 +6,11 @@
 package org.proto1.domain.order;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import org.proto1.domain.party.OrganizationUnit;
 
 /**
  * @author Valentin Pogrebinsky (pva@isd.com.ua)
@@ -15,5 +19,17 @@ import javax.persistence.Table;
 @Entity
 @Table(name="REQUEST")
 public class Request extends BaseOrder {
+	
+	@ManyToOne(optional=false)
+	@JoinColumn(name="ORG_UNIT_ID")
+	private OrganizationUnit organizationUnit;
+
+	public OrganizationUnit getOrganizationUnit() {
+		return organizationUnit;
+	}
+
+	public void setOrganizationUnit(OrganizationUnit organizationUnit) {
+		this.organizationUnit = organizationUnit;
+	}
 	
 }
