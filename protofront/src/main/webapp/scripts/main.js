0eccf36ce112efa60605f1dc7b2dc3079fcae45e
pva <pva@isd.comua>
2015-11-10 15:43:08 +0200

    GNU v2 Copyrights applied again

--- protofront/src/main/webapp/scripts/main.js
+++ protofront/src/main/webapp/scripts/main.js
@@ -1,10 +1,28 @@
+/*******************************************************************************
+ * Copyright (C) 2015   Valentin Pogrebinsky 
+ *
+ * mail:pva@isd.com.ua
+ * https://github.com/bbones
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * GNU v2 license text in root directory of project
+ *******************************************************************************/
 requirejs.config({
   'baseUrl': 'scripts',
   'paths': {
-    'jquery': '../lib/jquery-2.1.1',
+    'jquery': '../lib/jquery-2.1.4',
     'easyui': '../easyui/jquery.easyui.min',
-    'edatagrid': '../easyui/jquery.edatagrid',
-    'datagrid.excel': '../easyui/jquery.datagrid.excel',
+    'edatagrid': 'plugins/jjquery.edatagrid.fix',
+    'datagrid.excel': 'plugins/jquery.datagrid.excel',
     'easyui.form.ext': 'plugins/jquery.form.ext',
     'easyui.grid.ext': 'plugins/jquery.grid.ext',
     'serializeObject': 'plugins/jquery.serializeObject',
--- services/src/main/java/org/proto1/wfsample/SayHelloService.java
+++ services/src/main/java/org/proto1/wfsample/SayHelloService.java
@@ -1,3 +1,21 @@
+/*******************************************************************************
+ * Copyright (C) 2015   Boris Efimenko
+ *
+ * mail:Boris.Efimenko@isd.com.ua
+ * https://github.com/BorisEfimenko
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * GNU v2 license text in root directory of project
+ *******************************************************************************/
 package org.proto1.wfsample;
 
 import org.activiti.engine.impl.pvm.delegate.ActivityExecution;
